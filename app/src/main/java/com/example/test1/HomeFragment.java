407a1952110556abd567ff2af0ef34e108f12a87
Micheal <michealnkim-wm19@student.tarc.edu.my>
2020-08-24 00:34:21 +0800

    commit by ng

--- app/src/main/java/com/example/test1/HomeFragment.java
+++ app/src/main/java/com/example/test1/HomeFragment.java
@@ -2,18 +2,29 @@ package com.example.test1;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home,container,false);
     }
+
 }
--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.test1;
 import android.content.Intent;
 import android.media.Rating;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -23,11 +25,21 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     static final String STATE_FRAGMENT = "state_of_fragment";
+    LinearLayout Popular,Coming_soon,Now_showing;
+    private ViewPager2 viewPager2;
+    private Handler sliderHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,8 +64,83 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new PolicyFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_policy);
         }
+
+        viewPager2=findViewById(R.id.viewPagerImageSlider);
+        Popular = (LinearLayout)findViewById(R.id.Popular);
+        Coming_soon = (LinearLayout)findViewById(R.id.Coming_soon);
+        Now_showing= (LinearLayout)findViewById(R.id.Now_showing);
+        List<SliderItem> sliderItems=new ArrayList<>();
+        sliderItems.add(new SliderItem(R.drawable.digimon_adventure___last_evolution_kizuna));
+        sliderItems.add(new SliderItem(R.drawable.my_hero_academia___heros_rising));
+        sliderItems.add(new SliderItem(R.drawable.hitman___agent_jun));
+        sliderItems.add(new SliderItem(R.drawable.baba_yaga___terror_of_the_dark_forest));
+        sliderItems.add(new SliderItem(R.drawable.jumanji2));
+
+        viewPager2.setAdapter(new SliderAdapter(sliderItems,viewPager2));
+
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer=new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r=1-Math.abs(position);
+                page.setScaleY(0.85f + r * 0.15f);
+            }
+        });
+
+        viewPager2.setPageTransformer(compositePageTransformer);
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                sliderHandler.removeCallbacks(sliderRunnable);
+                sliderHandler.postDelayed(sliderRunnable,3000);
+            }
+        });
+
+
     }
 
+    public void Pop(View view){
+        Popular.setVisibility(View.VISIBLE);
+        Coming_soon.setVisibility(View.GONE);
+        Now_showing.setVisibility(View.GONE);
+    }
+    public void coming(View view){
+        Popular.setVisibility(View.GONE);
+        Coming_soon.setVisibility(View.VISIBLE);
+        Now_showing.setVisibility(View.GONE);
+    }
+    public void Now(View view){
+        Popular.setVisibility(View.GONE);
+        Coming_soon.setVisibility(View.GONE);
+        Now_showing.setVisibility(View.VISIBLE);
+    }
+
+    private Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem()+1);
+        }
+    };
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sliderHandler.removeCallbacks(sliderRunnable);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sliderHandler.postDelayed(sliderRunnable,3000);
+    }
 
     public void Rating(View view) {
         Log.d(LOG_TAG, "Button clicked!");
