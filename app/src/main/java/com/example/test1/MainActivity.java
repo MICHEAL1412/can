28bc81cab645ad8efba79846fc3976a8b10cb213
Micheal <michealnkim-wm19@student.tarc.edu.my>
2020-08-27 01:42:24 +0800

    commit by ng

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -31,6 +31,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     static final String STATE_FRAGMENT = "state_of_fragment";
 
+    LinearLayout Popular,Coming_soon,Now_showing;
+    private ViewPager2 viewPager2;
+    private Handler sliderHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,11 +51,53 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        viewPager2=findViewById(R.id.viewPagerImageSlider);
+        Popular = (LinearLayout)findViewById(R.id.Popular);
+        Coming_soon = (LinearLayout)findViewById(R.id.Coming_soon);
+        Now_showing= (LinearLayout)findViewById(R.id.Now_showing);
+        List<SliderItem> sliderItems=new ArrayList<>();
+        sliderItems.add(new SliderItem(R.drawable.digimon_adventure___last_evolution_kizuna));
+        sliderItems.add(new SliderItem(R.drawable.my_hero_academia___heros_rising));
+        sliderItems.add(new SliderItem(R.drawable.hitman___agent_jun));
+        sliderItems.add(new SliderItem(R.drawable.baba_yaga___terror_of_the_dark_forest));
+        sliderItems.add(new SliderItem(R.drawable.jumanji2));
+
+        viewPager2.setAdapter(new SliderAdapter(sliderItems,viewPager2));
+
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer=new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r=1-Math.abs(position);
+                page.setScaleY(0.85f + r * 0.15f);
+            }
+        });
+
+        viewPager2.setPageTransformer(compositePageTransformer);
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                sliderHandler.removeCallbacks(sliderRunnable);
+                sliderHandler.postDelayed(sliderRunnable,3000);
+            }
+        });
+
+
+
 
 
         if(savedInstanceState==null){
 
-
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FaqFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_faq);
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new PolicyFragment()).commit();
@@ -60,6 +106,43 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
 
+    }
+
+    public void Pop(View view){
+        Popular.setVisibility(View.VISIBLE);
+        Coming_soon.setVisibility(View.GONE);
+        Now_showing.setVisibility(View.GONE);
+    }
+    public void coming(View view){
+        Popular.setVisibility(View.GONE);
+        Coming_soon.setVisibility(View.VISIBLE);
+        Now_showing.setVisibility(View.GONE);
+    }
+    public void Now(View view){
+        Popular.setVisibility(View.GONE);
+        Coming_soon.setVisibility(View.GONE);
+        Now_showing.setVisibility(View.VISIBLE);
+    }
+
+    private Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem()+1);
+        }
+    };
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        sliderHandler.removeCallbacks(sliderRunnable);
+    }
+
+
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        sliderHandler.postDelayed(sliderRunnable,3000);
     }
 
     public void Rating(View view) {
@@ -101,6 +184,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch(item.getItemId()){
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+                break;
             case R.id.nav_faq:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FaqFragment()).commit();
                 break;
